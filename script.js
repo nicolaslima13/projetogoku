f255777b6538264cff7973ed8d615fbaae6d7495
nicolaslima13 <137187372+nicolaslima13@users.noreply.github.com>
2023-07-06 23:32:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const xButton = document.getElementById("xButton")
-const videoPopup = document.getElementById("videoPopup")
-const video = document.getElementById("video")
+const xButton = document.getElementById('xButton');
+const videoPopup = document.getElementById('videoPopup');
+const video = document.getElementById('video');
 
-xButton.addEventListener("click", function () {
-  videoPopup.style.display = "block"
-  video.play()
-})
+xButton.addEventListener('click', function() {
+  videoPopup.style.display = 'block';
+  video.muted = false; // Ativa o áudio
+  video.play();
+});
 
-video.addEventListener("ended", function () {
-  videoPopup.style.display = "none"
-})
+video.addEventListener('ended', function() {
+  videoPopup.style.display = 'none';
+});
 
-videoPopup.addEventListener("click", function (e) {
+videoPopup.addEventListener('click', function(e) {
   if (e.target === videoPopup) {
-    videoPopup.style.display = "none"
+    videoPopup.style.display = 'none';
   }
-})
+});
